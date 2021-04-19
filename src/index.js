13009af2b6b43ebe8bdaa0a2b9d2b06dade4b26f
alexyanchuk <alex7devel@gmail.com>
2021-04-19 22:51:30 +0300

    Merge conflict

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./views/App";
+import SimpleApp from "./views/SimpleApp";
+import PaginagionApp from "./views/PaginagionApp";
+import "./index.module.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+    <div className="app">
+        <SimpleApp />
+        {/* <PaginagionApp /> */}
+    </div>,
+    document.getElementById("app"),
+);
